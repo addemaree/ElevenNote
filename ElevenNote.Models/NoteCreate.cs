cbfe636181fbae3338ba5b2ef5fa221cc0726797
Adam Demaree <demareeadam1@gmail.com>
2017-04-12 15:41:31 -0400

    Add new creat model, controller edits, model value added

---
+++ ElevenNote.Models/NoteCreate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Models
+{
+    public class NoteCreate
+    {
+        [Required]
+        [MinLength(2, ErrorMessage ="Title needs to be at least 2 characters long.")]
+        [MaxLength(128, ErrorMessage ="Title is too long.")]
+        public string Title { get; set; }
+
+        [Required]
+        [MaxLength(8000, ErrorMessage ="We dont want to read your short novel.")]
+        public string Content { get; set; }
+
+        public override string ToString() => Title;
+        
+    }
+}
