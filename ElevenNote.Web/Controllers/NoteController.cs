9528c95d81e45b31a24824ae0e4d7cf814ba9fc2
Adam Demaree <demareeadam1@gmail.com>
2017-04-13 13:53:24 -0400

    Added Edit Feature

--- ElevenNote.Web/Controllers/NoteController.cs
+++ ElevenNote.Web/Controllers/NoteController.cs
@@ -49,6 +49,51 @@ namespace ElevenNote.Web.Controllers
             return View(model);
         }
 
+        public ActionResult Edit(int id)
+        {
+            var svc = CreateNoteService();
+            var detail = svc.GetNoteById(id);
+            var model = new NoteEdit
+            {
+                NoteId = detail.NoteId,
+                Title = detail.Title,
+                Content = detail.Content
+            };
+
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, NoteEdit model)
+        {
+            if (!ModelState.IsValid) return View(model);
+            
+            if(model.NoteId != id)
+            {
+                ModelState.AddModelError("", "Id Mismatch");
+                return View(model);
+            }
+
+            var service = CreateNoteService();
+
+            if (service.UpdateNote(model))
+            {
+                TempData["SaveResult"] = "Your note was updated!";
+                return RedirectToAction("Index");
+            }
+
+            ModelState.AddModelError("", "Your note could not be updated.");
+            return View(model);
+            
+        }
+
+        //public ActionResult Delete ()
+        //{
+        //    return View();
+        //}
+
         private NoteService CreateNoteService()
         {
             var userId = Guid.Parse(User.Identity.GetUserId());
