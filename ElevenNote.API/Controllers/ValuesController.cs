93513eae9d3b2a340b4d832104dfd2e5a704d5c3
Adam Demaree <demareeadam1@gmail.com>
2017-04-25 09:55:22 -0400

    API project created

---
+++ ElevenNote.API/Controllers/ValuesController.cs
@@ -0,0 +1,43 @@
+﻿using ElevenNote.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ElevenNote.API.Controllers
+{
+    [Authorize]
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        public IHttpActionResult Get()
+        {
+            var service = new NoteService(Guid.Parse("4e48a103-36f6-487d-8f4b-89e2d73024d6"));
+            var data = service.GetNotes();
+            return Ok(data);
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
